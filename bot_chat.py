f4d854073ea68aa4597332dbc5dde46f4529a35b
Overlord <hf_kknopsikk@mail.ru>
2018-03-27 15:42:20 +0300

    bot chat

--- bot_chat.py
+++ bot_chat.py
@@ -4,66 +4,82 @@ import telebot
 import config
 import dbworker
 import RINNEGAN as RN
-
+ #state = dbworker.get_current_state(message.chat.id)
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=["reset"])
+def cmd_reset(message):
+    bot.send_message(message.chat.id, "Начнём сначала, введите /start")
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
 
 # Начало диалога
 @bot.message_handler(commands=["start"])
 def cmd_start(message):
-    state = dbworker.get_current_state(message.chat.id)
-    if state == config.States.S_ENTER_NAME.value:
-        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить своё имя, но так и не сделал этого :( Жду...")
-    elif state == config.States.S_ENTER_AGE.value:
-        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить свой возраст, но так и не сделал этого :( Жду...")
-    elif state == config.States.S_SEND_PIC.value:
-        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить картинку, но так и не сделал этого :( Жду...")
-    else:  # Под "остальным" понимаем состояние "0" - начало диалога
-        bot.send_message(message.chat.id, "Привет! Как я могу к тебе обращаться?")
-        dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
-
-
-# По команде /reset будем сбрасывать состояния, возвращаясь к началу диалога
-@bot.message_handler(commands=["reset"])
-def cmd_reset(message):
-    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Как тебя зовут?")
-    dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
-
-
-@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_NAME.value)
-def user_entering_name(message):
-    # В случае с именем не будем ничего проверять, пусть хоть "25671", хоть Евкакий
-    bot.send_message(message.chat.id, "Отличное имя, запомню! Теперь укажи, пожалуйста, свой возраст.")
-    dbworker.set_state(message.chat.id, config.States.S_ENTER_AGE.value)
-
-
-@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_AGE.value)
-def user_entering_age(message):
-    # А вот тут сделаем проверку
-    if not message.text.isdigit():
-        # Состояние не меняем, поэтому только выводим сообщение об ошибке и ждём дальше
-        bot.send_message(message.chat.id, "Что-то не так, попробуй ещё раз!")
-        return
-    # На данном этапе мы уверены, что message.text можно преобразовать в число, поэтому ничем не рискуем
-    if int(message.text) < 5 or int(message.text) > 100:
-        bot.send_message(message.chat.id, "Какой-то странный возраст. Не верю! Отвечай честно.")
-        return
-    else:
-        # Возраст введён корректно, можно идти дальше
-        bot.send_message(message.chat.id, "Когда-то и мне было столько лет...эх... Впрочем, не будем отвлекаться. "
-                                          "Отправь мне какую-нибудь фотографию.")
-        dbworker.set_state(message.chat.id, config.States.S_SEND_PIC.value)
-
-
-@bot.message_handler(content_types=["photo"],
-                     func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_SEND_PIC.value)
-def user_sending_photo(message):
-    # То, что это фотография, мы уже проверили в хэндлере, никаких дополнительных действий не нужно.
-    bot.send_message(message.chat.id, "Отлично! Больше от тебя ничего не требуется. Если захочешь пообщаться снова - "
-                     "отправь команду /start.")
+    bot.send_message(message.chat.id, "Я создан для того, чтобы служить вам, Владыка.")
+    bot.send_message(message.chat.id, "Если желаете найти что-нибудь на барахолках, введите /search")
+    bot.send_message(message.chat.id, "Если хотите, чтобы я отслеживал новые лоты, введите /watchdog")
+    bot.send_message(message.chat.id, "Если хотите познать множество тайн, введите /help")
     dbworker.set_state(message.chat.id, config.States.S_START.value)
+ 
+@bot.message_handler(commands=["help"],func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_START.value)
+def cmd_help(message):
+    bot.send_message(message.chat.id, "Я создан для того, чтобы служить вам, Владыка.")
 
+    
+@bot.message_handler(commands=["search"],func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_START.value)
+def cmd_search(message):
+    bot.send_message(message.chat.id, "Что желаете найти, мой Повелитель?")
+    dbworker.set_state(message.chat.id, config.States.S_SEARCH.value)
+    
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_SEARCH.value)
+def input_lot_name(message):
+    global lot_name
+    lot_name=message.text
+    dbworker.set_state(message.chat.id, config.States.S_RESULT.value)
+    bot.send_message(message.chat.id, "Отлично, если хотите увидеть всё, что я нашёл, введите /result")
+    bot.send_message(message.chat.id, "Если желаете вывести в порядке возрастания цены, введите /price")
+    bot.send_message(message.chat.id, "Если желаете вывести в заданном диапазоне цен, введите /price_filter")
+    bot.send_message(message.chat.id, "Если желаете активировать фильтр по названию, введите /name_filter")
+    
+@bot.message_handler(commands=["result"],func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_RESULT.value)
+def cmd_result(message):
+    global lot_name
+    OUT=RN.Items(lot_name)
+    OUT.full_result(message.chat.id)
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
+    
+@bot.message_handler(commands=["price"],func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_RESULT.value)
+def cmd_price(message):
+    global lot_name
+    OUT=RN.Items(lot_name)
+    OUT.sort()
+    OUT.full_result(message.chat.id)
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
+    
+@bot.message_handler(commands=["name_filter"],func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_RESULT.value)
+def cmd_name_filter(message):
+    global lot_name
+    OUT=RN.Items(lot_name)
+    OUT.get_name_filtered(message.chat.id)
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
+    
+@bot.message_handler(commands=["price_filter"],func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_RESULT.value)
+def cmd_price_filtered(message):
+    bot.send_message(message.chat.id, "Пожалуйста, введите цену в формате: минимальная_цена максимальная_цена")
+    dbworker.set_state(message.chat.id, config.States.S_PRICE_RANGE.value)
 
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_PRICE_RANGE.value)
+def out_price_range(message):   
+    try:
+        min_price=float(message.text.split(' ')[0])
+        max_price=float(message.text.split(' ')[1])
+    except:
+        min_price=0
+        max_price=10000
+    OUT=RN.Items(lot_name)
+    OUT.get_price_filtered(message.chat.id,min_price,max_price)
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
+    
 if __name__ == "__main__":
     bot.polling(none_stop=True)
 
