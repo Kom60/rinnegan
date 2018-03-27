05316cd3f6aee3cd4f5e68458ad0a04b4de3ea0f
Overlord <hf_kknopsikk@mail.ru>
2018-03-27 14:26:31 +0300

    fucking backups

---
+++ config.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 10:09:46 2018
+
+@author: root
+"""
+
+from enum import Enum
+
+token="550900373:AAGBco24Kw9sKCryxDFiGX657p8Adqc34mk"
+db_file = "database21111.vdb"
+
+class States(Enum):
+    S_START="0"
+    S_SEARCH="1"
+    S_WATCHDOG="2"
+    S_WATCHLIST="3"
+    S_HELP="4"
+    S_SET_PLACE="5"
+    S_FILTER="6"
+    S_FILTER_PRICE="7"
+    S_FILTER_NAME="8"
+    S_NO_FILTER="9"
+    S_WATCHLIST_DELETE="10"
+    S_WATCHLIST_ADD="11"
+    S_WATCHLIST_CLEAR="12"
+    S_RESULT="13"
+    S_PRICE_RANGE="14"
+    S_WATCHDOG_ON="15"
+'''
+class States(Enum):
+    S_ENTER_NAME="0"
+    S_ENTER_AGE="1"
+    S_SEND_PIC="2"
+    '''
---
+++ dbworker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 10:05:36 2018
+
+@author: root
+"""
+from vedis import Vedis
+import config
+
+def get_current_state(user_id):
+    with Vedis(config.db_file) as db:
+        try:
+            return db[user_id]
+        except KeyError:
+            return config.States.S_START.value
+    
+def set_state(user_id,value):
+    with Vedis(config.db_file) as db:
+        try:
+            db[user_id]=value
+            return True
+        except:
+            return False
+        
+        
